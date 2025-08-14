fa193b8ac18fe8d337177b7807adb65186830b8d
Jackie-chan7w7 <patita8669527@gmail.com>
2025-08-13 20:55:20 -0600

    DEPOOL estuvo aquí 🐏

--- plugins/grupo-admins.js
+++ plugins/grupo-admins.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, participants, groupMetadata, args }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './media/img/miniurl.mp4'
 const groupAdmins = participants.filter(p => p.admin)
-const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n💙 ')
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n💖 ')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
 let text = `
 ≡ *Admis del grupo* _${groupMetadata.subject}_
 
 ┌─⊷ *ADMINISTRADORES*
-💙 ${listAdmin}
+💖 ${listAdmin}
 └───────────
 `.trim()
 conn.sendFile(m.chat, pp, 'staff.png', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
