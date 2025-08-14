c9641878af424c688c263c21f7d11407c06ac16d
Jackie-chan7w7 <patita8669527@gmail.com>
2025-08-13 19:35:36 -0600

    DEPOOL estuvo aquí 🐏

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `💙 Chat Baneado con exito.`, m, rcanal)
-    await m.react('✅')
+    await conn.reply(m.chat, `💜 Chat Pecador Baneado con exito.`, m, rcanal)
+    await m.react('🐐')
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
+handler.command = ['banearbot', 'sinner']
 handler.group = true 
 export default handler
