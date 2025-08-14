876b34f46cff304a29fd830fc8dc84fb3adca48b
Jackie-chan7w7 <patita8669527@gmail.com>
2025-08-14 14:56:30 -0600

    DEPOOL estuvo aquí 🐏

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -2,20 +2,20 @@ import db from '../lib/database.js'
 
 let handler = async (m, { conn, usedPrefix }) => {
     let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-    if (who == conn.user.jid) return m.react('🎤')
-    if (!(who in global.db.data.users)) return m.reply(`🎤💙 El usuario no se encuentra en la base de datos del concierto virtual ✨`)
+    if (who == conn.user.jid) return m.react('💌')
+    if (!(who in global.db.data.users)) return m.reply(`😴✨ El usuario no se encuentra en la base de datos de la Tabetna del Cerdo 😴✨`)
   
     let user = global.db.data.users[who]
     let total = (user.coin || 0) + (user.bank || 0);
 
-    const texto = `🎤💙 Información Económica del Concierto Virtual ✨
+    const texto = `💜🌠 Información Económica del Pecador 💜🌠
 
-🎵 Fanático » *${conn.getName(who)}*   
-💎 Notas Musicales » *${user.coin} ${moneda}*
+🐉 Pecado » *${conn.getName(who)}*   
+📿 Marionetas » *${user.coin} ${moneda}*
 🏦 Banco Virtual » *${user.bank} ${moneda}*
-✨ Total » *${total} ${moneda}*
+🧸 Total » *${total} ${moneda}*
 
-🎤💙 *¡Para proteger tus notas musicales, depósitalas en el banco virtual usando #deposit!* ✨🎵`;
+🤫 *¡Para proteger tus marionetas, depósitalas en el banco virtual usando #deposit!* ⚡`;
 
     await conn.reply(m.chat, texto, m)
 }
