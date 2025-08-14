53ed8deb337516808e7a61d47fdd0715efd23b28
Jackie-chan7w7 <patita8669527@gmail.com>
2025-08-13 20:49:53 -0600

    DEPOOL estuvo aquí 🐏

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -6,6 +6,6 @@ let handler = async (m, { conn, isAdmin, isROwner }) => {
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
-handler.command = ['banearbot', 'sinner']
+handler.command = ['banchat', 'sinner']
 handler.group = true 
 export default handler
