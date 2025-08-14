0a809c68ba0569df4e4f3ecc2c9e424cca935638
Jackie-chan7w7 <patita8669527@gmail.com>
2025-08-14 12:09:05 -0600

    DEPOOL estuvo aquí 🐏

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '❤ Bot activo en este grupo.', m, rcanal)
-    await m.react('✅')
+    await conn.reply(m.chat, '💜 Gowther activo en el grupo.', m, rcanal)
+    await m.react('🐐')
 }
 handler.help = ['desbanearbot']
 handler.tags = ['group']
-handler.command = ['desbanearbot', 'unbanchat']
+handler.command = ['arcangel', 'unbanchat']
 handler.group = true 
 export default handler
