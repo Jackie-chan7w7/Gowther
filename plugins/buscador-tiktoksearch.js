d2d31e91a4ec921e07c3b36147680b75c90c3255
Jackie-chan7w7 <patita8669527@gmail.com>
2025-08-15 14:18:11 -0600

    DEPOOL estuvo aquí 🐏

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -9,7 +9,7 @@ const {
 
 let handler = async (message, { conn, text, usedPrefix, command }) => {
   if (!text) {
-    return conn.reply(message.chat, "💙 Por favor, ingrese un texto para realizar una búsqueda en tiktok.", message, rcanal);
+    return conn.reply(message.chat, "🩷 Por favor, ingrese un texto para realizar una búsqueda en tiktok.", message, rcanal);
   }
 
   async function createVideoMessage(url) {
@@ -29,13 +29,13 @@ let handler = async (message, { conn, text, usedPrefix, command }) => {
   }
 
   try {
-    conn.reply(message.chat, '💙 *ENVIANDO SUS RESULTADOS..*', message, {
+    conn.reply(message.chat, '🩷 *ENVIANDO SUS RESULTADOS..*', message, {
       contextInfo: { 
         externalAdReply: { 
           mediaUrl: null, 
           mediaType: 1, 
           showAdAttribution: true,
-          title: '🅼🅸🅺🆄',
+          title: '🅶🅾🆆🆃🅷🅴🆁',
           body: dev,
           previewType: 0, 
           thumbnail: avatar,
