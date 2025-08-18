084a5986eb9f622e4d56c9d55d6e7dbd8c14173e
Jackie-chan7w7 <patita8669527@gmail.com>
2025-08-18 14:23:52 -0600

    DEPOOL estuvo aquí 🐏

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
   const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}  
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz1lR8c_Yz-RM_GQTJFJc18W-VT7mxMVNHsRYgr1wj1USPmM4YiEgkHym0&s=10.png')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9rIwlEnrxbyemvbtbokz7Rd5DC_1wqavseFCThNPmFwcCsGIQoHetKqtb&s=10.png')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
   let txt = '🐐 ¡Un Nuevo Pecador a Llegado ! 🐐'
@@ -17,7 +17,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `♥️*¡Que onda!* Bienvenido al Bar el Dragon del Sol 🐉 ${groupMetadata.subject}🐐\n\n♥️ @${m.messageStubParameters[0].split`@`[0]} 🐐\n\n🐐 ${global.welcom1} ✨\n\n♥️ ¡Ahora somos ${groupSize} titeres de Gowther! 🦖\n\n🦖 ¡Prepárate para la lucha! (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)⁠❤ 📿\n\n> 📿 Usa *#help* para ver todos los pecados de Gowther! ✨\n SIGUE NUESTRO CANAL \n https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o\n> NADJA MI ESPOSA`    
+    let bienvenida = `♥️*¡Que onda!* Bienvenido al Bar el Dragon del Sol 🐉 de ${groupMetadata.subject}🐐\n\n♥️ @${m.messageStubParameters[0].split`@`[0]} 🐐\n\n🐐 ${global.welcom1} ✨\n\n♥️ ¡Ahora somos ${groupSize} titeres de Gowther! 🦖\n\n🦖 ¡Prepárate para la lucha! (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)⁠❤ 📿\n\n> 📿 Usa *#help* para ver todos los pecados de Gowther! ✨\n SIGUE NUESTRO CANAL \n https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o\n> NADJA MI ESPOSA`    
     await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak, m, rcanal)
   }
   
