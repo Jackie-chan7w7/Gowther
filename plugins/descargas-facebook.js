00753400d067c2376e411f53c0781a454a0ef50e
Jackie-chan7w7 <patita8669527@gmail.com>
2025-08-18 16:10:04 -0600

    DEPOOL estuvo aquí 🐏

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -31,7 +31,7 @@ const handler = async (m, { text, conn, args }) => {
 
   let video = data.url;
   try {
-    await conn.sendMessage(m.chat, { video: { url: video }, caption: `😍${emoji} Aqui tienes (⁠づ⁠｡⁠◕⁠‿⁠‿⁠◕⁠｡⁠)⁠づ.`, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
+    await conn.sendMessage(m.chat, { video: { url: video }, caption: `😍 Aqui tienes (⁠づ⁠｡⁠◕⁠‿⁠‿⁠◕⁠｡⁠)⁠づ.`, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
     await m.react(done);
   } catch (e) {
     return conn.reply(m.chat, `${msm} Error al enviar el video.`, m)
